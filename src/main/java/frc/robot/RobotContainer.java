76d5dcbd3edccbfaf8d917dadd34e20528ff2037
Elliot Holmes <elliot.b.holmes@gmail.com>
2024-07-22 20:51:45 -0400

    removed comp bot code and added a command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,45 +4,11 @@
 
 package frc.robot;
 
-import frc.robot.commands.AdjustNote;
-import frc.robot.commands.Autos;
-import frc.robot.commands.Teleop.CleanIntake;
-import frc.robot.commands.Teleop.BringIn;
-import frc.robot.commands.Teleop.Climb;
-import frc.robot.commands.Teleop.PodiumShooterPreset;
-import frc.robot.commands.Teleop.FloorPickup;
-import frc.robot.commands.Teleop.FullIntake;
-import frc.robot.commands.Autoscommands.IntakeOut;
-import frc.robot.commands.Teleop.Intakeshoot;
-import frc.robot.commands.Teleop.Shoot;
-import frc.robot.commands.Teleop.ShooterAlignAmp;
-import frc.robot.commands.Teleop.ShooterDown;
-import frc.robot.commands.Teleop.ShooterFeed;
-import frc.robot.commands.Teleop.ShooterPivotPreset;
-import frc.robot.commands.Teleop.Shooterarm;
-import frc.robot.commands.Teleop.StowIntake;
-import frc.robot.commands.ResetGyro;
-import frc.robot.commands.Autoscommands.IntakeAutos;
-import frc.robot.commands.Autoscommands.ManualControlIntake;
-import frc.robot.commands.Autoscommands.ShootAuto;
-import frc.robot.commands.Teleop.TeleopSwerve;
-import frc.robot.commands.Teleop.Transition;
-import frc.robot.commands.Teleop.intakeMove;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.Alert;
 import frc.robot.util.Alert.AlertType;
 
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
-import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.PivotIntake;
-import frc.robot.subsystems.PivotShooter;
-import frc.robot.subsystems.Shooter;
-
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
@@ -65,135 +31,22 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  public static CTREConfigs ctreConfigs = new CTREConfigs();
-
-  // subsystems
-  public final PivotShooter pShooter = new PivotShooter();
-  public final Shooter shooter = new Shooter();
-  public final PivotIntake pIntake = new PivotIntake();
-  public final Intake intake = new Intake();
-  public final Drivetrain drivetrain = new Drivetrain();
-  public final Climber leftClimber = new Climber(Constants.ClimberConstants.LEFT, "Left", true);
-  public final Climber rightClimber = new Climber(Constants.ClimberConstants.RIGHT, "Right", false);
-  public final Feeder feeder = new Feeder();
-
-  // Braden failing to code
-
-  public final double arm_zero = 0.0;
-
   // Controllers
-  private final Joystick driver = new Joystick(0);
-  final Joystick operator = new Joystick(1);
-
-  // operator buttons
-  public final POVButton operatorDpadUp = new POVButton(operator, 0);
-  public final POVButton operatorDpadRight = new POVButton(operator, 90);
-  public final POVButton operatorDpadDown = new POVButton(operator, 180);
-  public final POVButton operatorDpadLeft = new POVButton(operator, 270);
-
-  // Drive Controls
-  private final int translationAxis = XboxController.Axis.kLeftY.value;
-  private final int strafeAxis = XboxController.Axis.kLeftX.value;
-  private final int rotationAxis = XboxController.Axis.kRightX.value;
-
-  // Driver Buttons
-  private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
-  private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+  private final Joystick tester = new Joystick(0);
 
   // Alerts
-  private final Alert driverDisconnectedAlert = new Alert("Driver controller is disconnected (port " + driver.getPort() + ").", AlertType.WARNING);
-  private final Alert operatorDisconnectedAlert = new Alert("Operator controller is disconnected (port " + operator.getPort() + ").", AlertType.WARNING);
+  private final Alert disconnectedAlert = new Alert("Controller is disconnected (port " + driver.getPort() + ").", AlertType.WARNING);
 
-  // BooleanSuppliers
-  private final BooleanSupplier rightTrigger = () -> operator.getRawAxis(XboxController.Axis.kRightTrigger.value) > Short.MAX_VALUE - 10;
-  private final BooleanSupplier leftTrigger = () -> operator.getRawAxis(XboxController.Axis.kLeftTrigger.value) > Short.MAX_VALUE - 10;
-  private final BooleanSupplier rightBumper = () -> operator.getRawAxis(XboxController.Button.kRightBumper.value) == 1;
-  private final BooleanSupplier leftBumper = () -> operator.getRawAxis(XboxController.Button.kLeftBumper.value) == 1;
-  private final BooleanSupplier toggleManual = () -> operator.getRawAxis(XboxController.Button.kStart.value) == 1; 
-  private  BooleanSupplier b_intakeMovement = () -> Math.abs(operator.getRawAxis(XboxController.Axis.kLeftY.value)) > 0;
-  private  BooleanSupplier b_intakein_out = () -> Math.abs(operator.getRawAxis(XboxController.Axis.kLeftX.value)) > 0;
-  private final BooleanSupplier b_shooterarm = () -> Math.abs(operator.getRawAxis(XboxController.Axis.kRightY.value)) != 0;
-
-  // Double Suplpliers
-  private final DoubleSupplier intakeMovement = () -> operator.getRawAxis(XboxController.Axis.kLeftY.value);
-  private final DoubleSupplier intakein_out = () -> operator.getRawAxis(XboxController.Axis.kLeftX.value);
-  private final DoubleSupplier shooterarm = () -> operator.getRawAxis(XboxController.Axis.kRightY.value);
-  private boolean Toggle;
-
-  // autos
-  
-  private final SendableChooser<Command> autoChooser;
-
-
-  
-  // climber Controls speeds
-  double climberspeed = -.05;
- 
   // The container for the robot. Contains subsystems, OI devices, and commands.
   public RobotContainer() {
-
-    pShooter.setEncoderPos(0.0);
-    pIntake.setEncoderPos(0.0);
 
     DriverStation.silenceJoystickConnectionWarning(true);
     // Setup Logging
     DataLogManager.start();
     DriverStation.startDataLog(DataLogManager.getLog());
-    Toggle = false; 
-    drivetrain.setDefaultCommand(
-      new TeleopSwerve(
-        drivetrain, 
-        () -> -driver.getRawAxis(translationAxis), 
-        () -> -driver.getRawAxis(strafeAxis), 
-        () -> -driver.getRawAxis(rotationAxis), 
-        () -> robotCentric.getAsBoolean()
-      )
-    );
-    pIntake.setDefaultCommand(
-      new intakeMove(pIntake, intakeMovement));
     
-    //intake.setDefaultCommand(
-    //   new Intakeshoot(intake, intakein_out));
-
-    pShooter.setDefaultCommand(
-      new Shooterarm(pShooter, shooterarm , arm_zero ));
-
-
-    // Braden's remode sick day code UNTESTED
-    NamedCommands.registerCommand("Floor pick up", new IntakeAutos(intake, pIntake));
-    NamedCommands.registerCommand("stow", new StowIntake(intake, pIntake));
-// imports needed 
-    NamedCommands.registerCommand("shoot", new ShootAuto(shooter, intake,feeder , "shoot"));
-
-    
-      // Configure the trigger bindings
+    // Configure the trigger bindings
     configureBindings();
-
-    // autos
-
-
-
-
-  
-
-
-    
-    // NamedCommands.registerCommand("BringIn" , new BringIn(pShooter, pIntake,));
-    // // eventMap.put("Climb" , new Climb( Climber , 1.0));
-    // // eventMap.put("Intake Out" , new IntakeOut(pIntake , pow));
-    // // eventMap.put("Intake In", new IntakeIn(pIntake , pow));
-    // NamedCommands.registerCommand("Intake Shoot", new Intakeshoot(intake , pow));
-    // NamedCommands.registerCommand("Reset Gyro" , new ResetGyro(drivetrain));
-    // NamedCommands.registerCommand("Shoot" , new Shoot( shooter , 1));
-    // NamedCommands.registerCommand("Shooter Align Amp" , new ShooterAlignAmp(pShooter));
-    // // NamedCommands.registerCommand("Shooter Arm Up" , new Shooterarm(pShooter , pow));
-    // // NamedCommands.registerCommand("Shooter Arm Down" , new Shooterarm(pShooter, pow));
-    // NamedCommands.registerCommand("Transition" , new Transition(feeder, rotationAxis));
- 
-
-    autoChooser = AutoBuilder.buildAutoChooser();
-    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   /**
@@ -210,61 +63,14 @@ public class RobotContainer {
     //new Trigger(m_exampleSubsystem::exampleCondition)
     //    .onTrue(new ExampleCommand(m_exampleSubsystem));
     // used to swicth the climber going up 0or down
-
-    // Shooter
-    new JoystickButton(operator, XboxController.Button.kA.value)
-     .whileTrue(new Shoot(shooter , 1));
-    new JoystickButton(operator, XboxController.Button.kB.value)
-     .whileTrue(new Shoot(shooter , -1 ));
-    new JoystickButton(operator, XboxController.Button.kX.value)
-     .whileTrue(new ShooterFeed(feeder, intake, 1));
-    new JoystickButton(operator, XboxController.Button.kY.value)
-      .onTrue(new AdjustNote(feeder, intake));
-    
-    operatorDpadUp.onTrue(new ShooterPivotPreset(pShooter, Constants.ShooterConstants.PODIUM));
-    operatorDpadLeft.onTrue(new ShooterPivotPreset(pShooter, 0.0));
-    operatorDpadRight.onTrue(new ShooterPivotPreset(pShooter, Constants.ShooterConstants.UP_LIMIT));
-    
-    // magic intake
-    new Trigger(() -> (driver.getRawAxis(XboxController.Axis.kLeftTrigger.value) > 0))
-      .whileTrue(new FullIntake(intake, pIntake, feeder, shooter))
-      .onFalse(new StowIntake(intake, pIntake));
-      //.whileTrue(new FloorPickup(intake, pIntake));
-      //.whileFalse(new StowIntake(intake, pIntake));
-    
-    // clean intake
-    new JoystickButton(driver, XboxController.Button.kA.value)
-      .whileTrue(new CleanIntake(pIntake, intake))
-      .onFalse(new StowIntake(intake, pIntake));
-
-    // climber 
-    new JoystickButton(operator, XboxController.Button.kLeftBumper.value)
-      .whileTrue(new Climb(leftClimber, -Constants.ClimberConstants.CLIMBER_SPEED));
-    
-    new Trigger(() -> (operator.getRawAxis(XboxController.Axis.kLeftTrigger.value) > 0))
-      .whileTrue(new Climb(leftClimber, Constants.ClimberConstants.CLIMBER_SPEED));
-
-    new JoystickButton(operator, XboxController.Button.kRightBumper.value)
-      .whileTrue(new Climb(rightClimber, Constants.ClimberConstants.CLIMBER_SPEED));
-    
-    new Trigger(() -> (operator.getRawAxis(XboxController.Axis.kRightTrigger.value) > 0))
-      .whileTrue(new Climb(rightClimber, -Constants.ClimberConstants.CLIMBER_SPEED));
-    
-    new JoystickButton(driver, XboxController.Button.kLeftBumper.value)
-      .onTrue(new ResetGyro(drivetrain));
   }
   
-
-
   /*
    * Checks if the controllers are plugged in and updates their respective alerts
    */
   public void checkControllers() {
-    boolean driverConnected = DriverStation.isJoystickConnected(driver.getPort());
-    boolean operatorConnected = DriverStation.isJoystickConnected(operator.getPort());
-
-    driverDisconnectedAlert.set(!driverConnected);
-    operatorDisconnectedAlert.set(!operatorConnected);
+    boolean connected = DriverStation.isJoystickConnected(tester.getPort());
+    disconnectedAlert.set(!connected);
   }
 
   /**
@@ -273,8 +79,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-   //  return null; // Autos.exampleAuto(m_exampleSubsystem);
-    return autoChooser.getSelected();
+    return null;
   }
-}
+} 
